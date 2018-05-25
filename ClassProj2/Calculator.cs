d0385d69174dee7194b8fc8301439bee4d9383e4
kim-young <younghyun87@gmail.com>
2018-05-25 16:12:27 -0400

    Overloading Methods

---
+++ ClassProj2/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ClassProj2
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+        }
+
+        public static int Addition(int number)
+		{
+			int result = number + 100;
+			return result;
+		}
+
+		public static double Addition(double number)
+		{
+			double result = (double)number + 200;
+			return result;
+		}
+    }
+}
---
+++ ClassProj2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassProj2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			Console.WriteLine("Input number.");
+			double inputNumber = double.Parse(Console.ReadLine());
+
+			Console.WriteLine(Calculator.Addition(inputNumber));
+			Console.ReadLine();
+		}
+    }
+}
