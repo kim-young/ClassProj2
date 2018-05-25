1c9112af0bab4b1fd56de9d4fa5dc68223c58d81
kim-young <younghyun87@gmail.com>
2018-05-25 16:16:37 -0400

    Third method added

--- ClassProj2/Calculator.cs
+++ ClassProj2/Calculator.cs
@@ -12,11 +12,17 @@ namespace ClassProj2
 			int result = number + 100;
 			return result;
 		}
-
+        
 		public static double Addition(double number)
 		{
 			double result = (double)number + 200;
 			return result;
 		}
+
+		public static int Addition(string number)
+        {
+			int result = int.Parse(number) + 500;
+            return result;
+        }
     }
 }
